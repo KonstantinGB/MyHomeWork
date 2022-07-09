67f472cbcc89f922a4939e5684d7a074075a32fe
Konstantin <osipo38@yandex.ru>
2022-07-09 11:52:01 +0300

    home work seminar 1

---
+++ Exercise1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Для нахождения максимального из трёх чисел введите их");
+
+int num1, num2, num3;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third integer number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max) max=num2;
+if(num3 > max) max=num3;
+
+Console.WriteLine("Наибольшее число " + max);
+
+
+
+
---
+++ Exercise1_4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введиче число что бы узнать все четные числа от 1 до вашего");
+
+int num;
+
+Console.Write("Input integer number > 1: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+if(num > 1)
+{
+    while(current <= num)
+    {
+        Console.Write(current + " ");
+        current = current + (2);
+    }
+}
+else
+    Console.Write("Вы ввели число равное 1 или меньше");
